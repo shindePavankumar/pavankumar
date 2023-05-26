b0834acee740bfff2756d77fd139cb0760b94b5f
shindePavankumar <shindepavankumar88@gmail.com>
2023-05-26 22:25:53 +0530

    Updated

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('hello world')
+// console.warn("warning")
+// console.error("Error is here")
+// document.write("hi this is document")  
+
+
+
+var num1=5;
+var num2=5;
+console.log(num1 +num2)
